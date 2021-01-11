8e1da7ed71a13d77f68d133a01513b5b53d76bf8
Tobias Plum <tobias@fam-plum.de>
2021-01-11 22:13:51 +0100

    script: added request of appointments

--- script.py
+++ script.py
@@ -3,6 +3,11 @@ import datetime
 import json
 
 dateUrl = "https://www.tuv.com/tos-pti-relaunch-2019/rest/ajax/getVacanciesByMonth"
+appointmentUrl = "https://www.tuv.com/tos-pti-relaunch-2019/rest/ajax/getVacanciesByDay"
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json; charset=utf-8",
+}
 
 
 def genPayloadDates(
@@ -11,12 +16,14 @@ def genPayloadDates(
     return f'{{"locale":"de-DE","isLegacyTos":false,"filterMonth":{month},"filterYear":{year},"vehicleServices":[{{"id":{vehicleServicesId}}}],"vehicleType":{{"id":{vehicleTypeId}}},"vics":[{{"id":{locationId},"externalLocale":"de-DE"}}]}}'
 
 
+def genPayloadAppointments(
+    date, locationId, vehicleServicesId="4007", vehicleTypeId="44"
+):
+    return f'{{"locale":"de-DE","isLegacyTos":false,"date":{{"date":"{date.strftime("%Y-%m-%d")}"}},"vehicleServices":[{{"id":{vehicleServicesId}}}],"vehicleType":{{"id":{vehicleTypeId}}},"vics":[{{"id":{locationId},"externalLocale":"de-DE"}}]}}'
+
+
 def getAvaiblableDates(month, year, locationId):
     payload = genPayloadDates(month, year, locationId)
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json; charset=utf-8",
-    }
     response = requests.post(dateUrl, data=payload, headers=headers)
     response.close()
     res = response.json()
@@ -25,4 +32,26 @@ def getAvaiblableDates(month, year, locationId):
     for i in res["vacancies"]:
         date = datetime.datetime.strptime(i["date"], "%Y-%m-%d")
         dates.append(date)
-        print(date)
+
+    return dates
+
+
+def getAvaiblableAppointmentsByDate(date, locationId):
+    payload = genPayloadAppointments(date, locationId)
+    response = requests.post(appointmentUrl, data=payload, headers=headers)
+    response.close()
+    res = response.json()
+
+    appointments = []
+    for i in res["timetables"][0]["timeRangeVacancies"]:
+        for j in i["timeslots"]:
+            time = j["availableDates"]
+            if time != []:
+                timeObj = datetime.datetime.strptime(time[0], "%H:%M")
+                appointments.append(
+                    date.replace(
+                        hour=int(timeObj.strftime("%H")),
+                        minute=int(timeObj.strftime("%M")),
+                    )
+                )
+    return appointments
