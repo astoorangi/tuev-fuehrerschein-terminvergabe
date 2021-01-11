60d85bd3b508382953a044da2b3a3f5862976029
Tobias Plum <tobias@fam-plum.de>
2021-01-11 12:41:23 +0100

    script.py: Added getAvaiblableDates

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import requests
+import datetime
+import json
 
 dateUrl = "https://www.tuv.com/tos-pti-relaunch-2019/rest/ajax/getVacanciesByMonth"
 
@@ -7,3 +9,20 @@ def genPayloadDates(
     month, year, locationId, vehicleServicesId="4007", vehicleTypeId="44"
 ):  # default for theoretical driving test
     return f'{{"locale":"de-DE","isLegacyTos":false,"filterMonth":{month},"filterYear":{year},"vehicleServices":[{{"id":{vehicleServicesId}}}],"vehicleType":{{"id":{vehicleTypeId}}},"vics":[{{"id":{locationId},"externalLocale":"de-DE"}}]}}'
+
+
+def getAvaiblableDates(month, year, locationId):
+    payload = genPayloadDates(month, year, locationId)
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json; charset=utf-8",
+    }
+    response = requests.post(dateUrl, data=payload, headers=headers)
+    response.close()
+    res = response.json()
+
+    dates = []
+    for i in res["vacancies"]:
+        date = datetime.datetime.strptime(i["date"], "%Y-%m-%d")
+        dates.append(date)
+        print(date)
